b0f877751932b6330da5389ebdbc0a59a4e19f04
Iamzohaibali <j36740062@gmail.com>
2026-09-20 09:11:14 +0500

    uiewiu

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 // Root endpoint
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello from Express API testing I expr djfhj kjthjhtgj",
+    message: "Hello from Express API. It has been updated.",
     version: "1.0.0",
   });
 });
